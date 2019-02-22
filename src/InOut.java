dc7f924c78ba121b507a8649a52e429d5064c971
Olivier <olivier.chevalier@student.unamur.be>
2019-02-22 18:59:39 +0100

    Début

--- src/InOut.java
+++ src/InOut.java
@@ -1,3 +1,15 @@
 public class InOut {
 
+
+    String input = "src/test/resources/problem1/DiviserPourRegner_2.2.txt";
+    String[] result = main.problem_1(getFileText(input));
+
+
+    public static String[] problem_1_naive(String s_file) {
+        System.out.println("===");
+        System.out.println(s_file);
+        System.out.println("===");
+
+        return Probleme1.traitement(s_file, false);
+    }
 }
